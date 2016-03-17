a368eb45f40ef710777815baeded2a4981cd70c2
Florent Taine <florenttaine@MacBook-Pro-de-Florent.local>
2016-03-17 15:42:54 +0100

    bug fix

--- dueltime/HomeViewController.swift
+++ dueltime/HomeViewController.swift
@@ -213,14 +213,14 @@ class HomeViewController: UIViewController {
 
         }
  
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
+
+
+
 
